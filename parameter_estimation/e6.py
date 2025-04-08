122e40a8d930130828e4fb39d283e91209ec5a5b
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2025-04-08 09:12:16 +0200

    E6: Characterize particles

--- parameter_estimation/e6.py
+++ parameter_estimation/e6.py
@@ -1,12 +1,12 @@
 """
-# E5: Characterize bed.
+# E6: Characterize particles.
 
-- Parameters: bed porosity and axial dispersion
-- Flow sheet: Full system
-- Injected component: Blue dextran (2 MDa)
-- Concentration: 0.0005 mM
+- Parameters: particle porosity / total porosity, axial dispersion
+- Flow sheet: full system
+- Injected component: Acetone
+- Concentration: 171.2 mM (1 %)
 - Volume: 50e-9 m³
-- Eluent: A
+- Eluent: Low Salt
 - Measurement: UV
 
 Status: done
@@ -15,19 +15,19 @@ Status: done
 
 # %% Imports
 
-from CADETProcess.processModel import GeneralRateModel, LumpedRateModelWithPores
+from CADETProcess.processModel import LumpedRateModelWithPores
 
 from knauer import PulseInjection, KnauerExperimentalData
 from calibration import normalize_area
 from characterization import (
-    CharacterizeBed,
+    CharacterizeParticles,
     optimize,
 )
 from parameters import (
-    component_system_dextran,
+    component_system_acetone,
     knauer_system_options,
-    c_dextran,
-    n_sample_dextran,
+    c_acetone,
+    n_sample_acetone,
     time_offset,
     cycle_time_bed,
     flow_rate,
@@ -46,20 +46,25 @@ knauer_system_options = knauer_system_options.copy()
 knauer_system_options['ColumnModel'] = LumpedRateModelWithPores
 knauer_system_options['BindingModel'] = None
 
-start_peak = 3*60
-peak_max = 4*60
+start_peak = 7*60
+end_peak = 12*60
 
 
 def setup_reference():
     """Set up reference data."""
-    file_path = experimental_data_path / "e5.csv"
+    file_path = experimental_data_path / "e6.csv"
 
     knauer_data = KnauerExperimentalData(
         file_path=file_path,
         flow_rate=flow_rate,
         time_offset=time_offset,
     )
-    reference = normalize_area(knauer_data.uv_1, n_sample_dextran)
+    reference = normalize_area(
+        knauer_data.uv_1,
+        n_sample_acetone,
+        start=start_peak,
+        end=end_peak,
+    )
 
     return reference
 
@@ -67,11 +72,11 @@ def setup_reference():
 def setup_process():
     """Set up process."""
     return PulseInjection(
-        "e5",
-        component_system_dextran,
+        "e6",
+        component_system_acetone,
         knauer_system_options,
         c_buffer_a=[0],
-        c_sample=[c_dextran],
+        c_sample=[c_acetone],
         cycle_time=cycle_time_bed,
         flow_rate=flow_rate,
     )
@@ -80,11 +85,20 @@ def setup_process():
 def run_optimization(
     process,
     reference,
+    include_axial_dispersion=True,
     prior_branch_name=None,
     debug=False,
 ):
     """Run optimization."""
-    commit_message = "E5"
+    characterization_options = {
+        "include_particle_porosity": True,
+        "include_film_diffusion": False,
+        "include_pore_diffusion": False,
+        "include_axial_dispersion": include_axial_dispersion,
+        "component_index": 0,
+    }
+
+    commit_message = "E6"
     if prior_branch_name is None:
         commit_message += "_synthetic"
     elif prior_branch_name == "parameters_lukas":
@@ -92,35 +106,43 @@ def run_optimization(
 
     return optimize(
         process,
-        CharacterizationType=CharacterizeBed,
+        CharacterizationType=CharacterizeParticles,
         solution_path=solution_path,
         components=components,
         references=reference,
         metrics=metrics,
         start_times=start_peak,
-        end_times=peak_max,
+        end_times=end_peak,
+        characterization_options=characterization_options,
         optimizer_options=optimizer_options,
         prior_branch_name=prior_branch_name,
         commit_message=commit_message,
         debug=debug,
     )
 
-
 # %% Run optimization
 
-def main(prior_branch_name=None, debug=False):
-    # Setup reference data
-    reference = None if prior_branch_name is None else setup_reference()
-
+def main(prior_branch_name=None, include_axial_dispersion=True, debug=False):
     # Setup process
     process = setup_process()
 
+    # Setup reference data
+    reference = None if prior_branch_name is None else setup_reference()
+
     # Run optimization
-    return run_optimization(process, reference, prior_branch_name, debug)
+    return run_optimization(
+        process,
+        reference,
+        include_axial_dispersion,
+        prior_branch_name, debug
+    )
 
 
 if __name__ == "__main__":
+    include_axial_dispersion = True
     debug = False
     prior_branch_name = None
 
-    e5_optimization_results, posteriour_branch_name = main(prior_branch_name, debug)
+    e6_optimization_results, posteriour_branch_name = main(
+        prior_branch_name, include_axial_dispersion, debug
+    )
