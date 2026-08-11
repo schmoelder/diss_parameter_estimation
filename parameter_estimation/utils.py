fdd843044cd7593f533f42669add75191f3a20b1
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2026-08-11 12:43:36 +0200

    Remove branch name for comparison with film diffusion

--- parameter_estimation/utils.py
+++ parameter_estimation/utils.py
@@ -17,7 +17,6 @@ from knauer import KnauerSystemProcess
 repo = ProjectRepo(__file__)
 
 experimental_data_branch = "2025-11-23_15-36-31_main_ab9cb48"  # experiments
-parameters_branch_e7_film_diffusion = "2026-04-21_13-07-28_main_ab1f4f5"  # limiting film diffusion
 
 
 def get_experimental_data_path() -> os.PathLike:
